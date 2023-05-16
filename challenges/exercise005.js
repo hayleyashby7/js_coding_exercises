84eac0400adc3bf5951fbc1a46427c9cb8cc0f06
Hayley Ashby <hayleyashby7@gmail.com>
2023-05-16 09:43:19 +0100

    exercise005 - findNextNumber

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -1,7 +1,21 @@
 export const findNextNumber = (nums, n) => {
 	if (nums === undefined) throw new Error('nums is required');
 	if (n === undefined) throw new Error('n is required');
-	// Your code here!
+
+	// Get the index of n
+	const index = nums.indexOf(n);
+
+	switch (true) {
+		case index === -1:
+			// n is not found
+			return null;
+		case index === nums.length - 1:
+			// n is the last number in the array
+			return null;
+		default:
+			// return next value after n
+			return nums[index + 1];
+	}
 };
 
 export const count1sand0s = (str) => {
