8259c1b3599259f8d66ffa0c496aa59418341be0
Hayley Ashby <hayleyashby7@gmail.com>
2023-05-16 10:51:44 +0100

    Removed redundent if/else

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -6,9 +6,7 @@ export function getFillings(sandwich) {
 export function isFromManchester(person) {
 	if (person === undefined) throw new Error('person is required');
 
-	if (person.city === 'Manchester') {
-		return true;
-	} else return false;
+	return person.city === 'Manchester';
 }
 
 export function getBusNumbers(people) {
@@ -34,14 +32,8 @@ export function hasMPostCode(person) {
 	if (person === undefined) throw new Error('person is required');
 
 	if (person.address.postCode.startsWith('M')) {
-		// Postcode starts with M
-
-		if (isNaN(person.address.postCode.charAt(1))) {
-			// Next character is a letter - not an M postcode
-			return false;
-		}
-		// Next character is a number - valid M postcode
-		else return true;
+		// Postcode starts with M - is there a letter or number next?
+		return !isNaN(person.address.postCode.charAt(1));
 	}
 	// Postcode does not start with M
 	else return false;
