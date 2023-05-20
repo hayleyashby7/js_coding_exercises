71b391e93ab8a1ceb29d1c7e723c32190f4470f9
Hayley Ashby <hayleyashby7@gmail.com>
2023-05-20 11:36:36 +0100

    exercise006 - test isItPrime - basic prime number check

--- challenges/exercise006.js
+++ challenges/exercise006.js
@@ -53,6 +53,16 @@ export const getComplementaryDNA = (str) => {
  */
 export const isItPrime = (n) => {
 	if (n === undefined) throw new Error('n is required');
+	
+	// Cover edge cases first so we dont waste a for loop if not needed
+	if (n <= 1) return false;
+	if (n < 4) return true; 
+	if (n % 2 === 0) return false;
+
+	for (let i = 5; i <= Math.sqrt(n); i += 2) {
+		if (n % i === 0) return false;
+	}
+	return true;
 };
 
 /**
