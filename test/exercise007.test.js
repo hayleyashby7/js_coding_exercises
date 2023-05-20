dea62ba07b6c207eb532a596672b0ac9c9c2dd9f
Hayley Ashby <hayleyashby7@gmail.com>
2023-05-20 14:28:16 +0100

    exercise007 - test sumDigits - adjusted layout to match previous tests

--- test/exercise007.test.js
+++ test/exercise007.test.js
@@ -1,14 +1,15 @@
 import { sumDigits, createRange, getScreentimeAlertList, hexToRGB, findWinner } from '../challenges/exercise007-optional';
 
-describe("sumDigits", () => { 
-    test('takes number and returns sum of digits', () => { 
-        // Arrange
-        const num = 123;
-       
-        // Act
-        const result = sumDigits(num);
-        
-        // Assert
-        expect(result).toBe(6);
-    }); 
+describe('sumDigits', () => {
+	test('takes number and returns sum of digits', () => {
+		// Arrange
+		const num = 123;
+		const expected = 6;
+
+		// Act
+		const result = sumDigits(num);
+
+		// Assert
+		expect(result).toBe(expected);
+	});
 });
