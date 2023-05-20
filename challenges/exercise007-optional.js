4e708c82d891de115dcf3ce177d92c3528ee4130
Hayley Ashby <hayleyashby7@gmail.com>
2023-05-20 14:26:30 +0100

    exercise007 - test sumDigits - initial implementation

--- challenges/exercise007-optional.js
+++ challenges/exercise007-optional.js
@@ -8,6 +8,8 @@
  */
 export const sumDigits = (n) => {
 	if (n === undefined) throw new Error('n is required');
+
+	return [...n.toString()].reduce((a, b) => parseInt(a) + parseInt(b));
 };
 
 /**
@@ -21,10 +23,7 @@ export const sumDigits = (n) => {
 export const createRange = (start, end, step) => {
 	if (start === undefined) throw new Error('start is required');
 	if (end === undefined) throw new Error('end is required');
-	if (step === undefined)
-		console.log(
-			"FYI: Optional step parameter not provided. Remove this check once you've handled the optional step!"
-		);
+	if (step === undefined) console.log("FYI: Optional step parameter not provided. Remove this check once you've handled the optional step!");
 };
 
 /**
