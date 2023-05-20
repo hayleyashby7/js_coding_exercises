adbb47d30c1f7ea4b0b5c521a2ecf60e0735b14b
Hayley Ashby <hayleyashby7@gmail.com>
2023-05-20 13:32:23 +0100

    exercise006 - isItPrime - refactored to use more efficient primality test

--- challenges/exercise006.js
+++ challenges/exercise006.js
@@ -58,8 +58,9 @@ export const isItPrime = (n) => {
 	if (n === 2 || n === 3) return true;
 	if (n % 2 === 0 || n % 3 === 0 || n <= 1) return false;
 
-	for (let i = 5; i <= Math.sqrt(n); i += 2) {
-		if (n % i === 0) return false;
+	// Faster to use primes > 3 always being next to multiple of 6
+	for (let i = 5; i <= Math.sqrt(n); i += 6) {
+		if (n % i === 0 || n % (i + 2) === 0) return false;
 	}
 	return true;
 };
