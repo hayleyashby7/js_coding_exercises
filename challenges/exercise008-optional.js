7129ee972d88361bc79ad77cc5b45dc5266c4a76
Hayley Ashby <hayleyashby7@gmail.com>
2023-05-20 16:30:23 +0100

    exercise008 - highestNumber

--- challenges/exercise008-optional.js
+++ challenges/exercise008-optional.js
@@ -4,18 +4,21 @@
 
 export function printColours(colours) {
 	if (!colours) throw new Error('colour is required');
+
 	return [...colours];
 }
 
 export function shoppingList(list, newItem) {
 	if (!list) throw new Error('list is required');
 	if (!newItem) throw new Error('newItem is required');
+
 	return [...list, ...newItem];
 }
 
 export function highestNumber(numbers) {
 	if (!numbers) throw new Error('numbers are required');
-	// Your solution using the spread operator here!
+
+	return Math.max(...numbers);
 }
 
 export function splitThatString(string) {
